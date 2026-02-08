90ff7307b6c8c37d28370cdf6caba048994a5cf0
deliyim3434 <104919540+deliyim3434@users.noreply.github.com>
2026-02-08 21:41:43 +0000

    𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ + #⟶̽ जय श्री ༢།म >𝟑🙏🚩

--- AloneMusic/plugins/admins/stop.py
+++ AloneMusic/plugins/admins/stop.py
@@ -10,7 +10,9 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    filters.command(["end","son","kapat", "stop", "cend", "cstop"]) & filters.group & ~BANNED_USERS
+    filters.command(["end", "son", "kapat", "stop", "cend", "cstop"])
+    & filters.group
+    & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
