9118417fd4be43902e5655a94c688276e75b5480
Ali <104919540+deliyim3434@users.noreply.github.com>
2026-02-09 01:06:25 +0300

    Update play.py

--- AloneMusic/plugins/play/play.py
+++ AloneMusic/plugins/play/play.py
@@ -1,10 +1,3 @@
-#
-# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
-#
-# This file is part of < https://github.com/TheAloneTeam/AloneMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
-# All rights reserved.
 
 import asyncio
 import random
@@ -61,10 +54,10 @@ async def delete_after_delay(msg):
 @app.on_message(
     filters.command(
         [
-            "play",
-            "vplay",
-            "cplay",
-            "cvplay",
+            "play","oynat",
+            "vplay","voynat",
+            "cplay","coynat",
+            "cvplay","cvoynat",
             "playforce",
             "vplayforce",
             "cplayforce",
