92316f8b98a13e477e185da60ba3ec2f0cd64b95
Ali <104919540+deliyim3434@users.noreply.github.com>
2026-02-09 00:44:04 +0300

    Update userbot.py

--- AloneMusic/core/userbot.py
+++ AloneMusic/core/userbot.py
@@ -7,14 +7,11 @@
 # All rights reserved.
 
 from pyrogram import Client
-
 import config
-
 from ..logging import LOGGER
 
 assistants = []
 assistantids = []
-
 
 class Userbot(Client):
     def __init__(self):
@@ -55,114 +52,90 @@ class Userbot(Client):
         )
 
     async def start(self):
-        LOGGER(__name__).info("Starting Assistants...")
+        LOGGER(__name__).info("Asistanlar Başlatılıyor...")
+        
         if config.STRING1:
             await self.one.start()
-            try:
-                await self.one.join_chat("TheAloneTeam")
-                await self.one.join_chat("Aashikteam")
-            except:
-                pass
             assistants.append(1)
             try:
-                await self.one.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.one.send_message(config.LOGGER_ID, "Asistan Başlatıldı")
             except:
                 LOGGER(__name__).error(
-                    "Assistant Account 1 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
+                    "Asistan Hesabı 1 log grubuna erişemedi. Asistanı log grubuna eklediğinizden ve yönetici yetkisi verdiğinizden emin olun!"
                 )
                 exit()
             self.one.id = self.one.me.id
             self.one.name = self.one.me.mention
             self.one.username = self.one.me.username
             assistantids.append(self.one.id)
-            LOGGER(__name__).info(f"Assistant Started as {self.one.name}")
+            LOGGER(__name__).info(f"Asistan {self.one.name} olarak başlatıldı")
 
         if config.STRING2:
             await self.two.start()
-            try:
-                await self.two.join_chat("Aashikteam")
-                await self.one.join_chat("TheAloneTeam")
-            except:
-                pass
             assistants.append(2)
             try:
-                await self.two.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.two.send_message(config.LOGGER_ID, "Asistan Başlatıldı")
             except:
                 LOGGER(__name__).error(
-                    "Assistant Account 2 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
+                    "Asistan Hesabı 2 log grubuna erişemedi. Asistanı log grubuna eklediğinizden ve yönetici yetkisi verdiğinizden emin olun!"
                 )
                 exit()
             self.two.id = self.two.me.id
             self.two.name = self.two.me.mention
             self.two.username = self.two.me.username
             assistantids.append(self.two.id)
-            LOGGER(__name__).info(f"Assistant Two Started as {self.two.name}")
+            LOGGER(__name__).info(f"Asistan İki {self.two.name} olarak başlatıldı")
 
         if config.STRING3:
             await self.three.start()
-            try:
-                await self.three.join_chat("Aashikteam")
-                await self.one.join_chat("TheAloneTeam")
-            except:
-                pass
             assistants.append(3)
             try:
-                await self.three.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.three.send_message(config.LOGGER_ID, "Asistan Başlatıldı")
             except:
                 LOGGER(__name__).error(
-                    "Assistant Account 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                    "Asistan Hesabı 3 log grubuna erişemedi. Asistanı log grubuna eklediğinizden ve yönetici yetkisi verdiğinizden emin olun!"
                 )
                 exit()
             self.three.id = self.three.me.id
             self.three.name = self.three.me.mention
             self.three.username = self.three.me.username
             assistantids.append(self.three.id)
-            LOGGER(__name__).info(f"Assistant Three Started as {self.three.name}")
+            LOGGER(__name__).info(f"Asistan Üç {self.three.name} olarak başlatıldı")
 
         if config.STRING4:
             await self.four.start()
-            try:
-                await self.four.join_chat("TheAloneTeam")
-                await self.one.join_chat("Aashikteam")
-            except:
-                pass
             assistants.append(4)
             try:
-                await self.four.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.four.send_message(config.LOGGER_ID, "Asistan Başlatıldı")
             except:
                 LOGGER(__name__).error(
-                    "Assistant Account 4 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                    "Asistan Hesabı 4 log grubuna erişemedi. Asistanı log grubuna eklediğinizden ve yönetici yetkisi verdiğinizden emin olun!"
                 )
                 exit()
             self.four.id = self.four.me.id
             self.four.name = self.four.me.mention
             self.four.username = self.four.me.username
             assistantids.append(self.four.id)
-            LOGGER(__name__).info(f"Assistant Four Started as {self.four.name}")
+            LOGGER(__name__).info(f"Asistan Dört {self.four.name} olarak başlatıldı")
 
         if config.STRING5:
             await self.five.start()
-            try:
-                await self.five.join_chat("TheAloneTeam")
-                await self.one.join_chat("Aashikteam")
-            except:
-                pass
             assistants.append(5)
             try:
-                await self.five.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.five.send_message(config.LOGGER_ID, "Asistan Başlatıldı")
             except:
                 LOGGER(__name__).error(
-                    "Assistant Account 5 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                    "Asistan Hesabı 5 log grubuna erişemedi. Asistanı log grubuna eklediğinizden ve yönetici yetkisi verdiğinizden emin olun!"
                 )
                 exit()
             self.five.id = self.five.me.id
             self.five.name = self.five.me.mention
             self.five.username = self.five.me.username
             assistantids.append(self.five.id)
-            LOGGER(__name__).info(f"Assistant Five Started as {self.five.name}")
+            LOGGER(__name__).info(f"Asistan Beş {self.five.name} olarak başlatıldı")
 
     async def stop(self):
-        LOGGER(__name__).info("Stopping Assistants...")
+        LOGGER(__name__).info("Asistanlar Durduruluyor...")
         try:
             if config.STRING1:
                 await self.one.stop()
